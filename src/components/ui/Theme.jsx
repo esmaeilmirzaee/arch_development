d844a279ffbada3ab037a5a9666bc8c5116f904b
EsmaeilMIRZAEE <EsmaeilMIRZAEE@gmail.com>
2020-08-12 02:49:53 -0400

    Main colour

--- src/components/ui/Theme.jsx
+++ src/components/ui/Theme.jsx
@@ -1,3 +1,25 @@
 import { createMuiTheme } from '@material-ui/core/styles';
 
-export default createMuiTheme({});
+const darkBlue = "#9381FF"
+const lightBlue = "#B8B8FF"
+const silver = "#F8F7FF"
+const lightCream = "#FFEEDD"
+const darkCream = "#FFD8BE"
+
+export default createMuiTheme({
+  palette: {
+    common: {
+      darkBlue = `${darkBlue}`,
+      lightBlue = `${lightBlue}`,
+      silver = `${silver}`,
+      lightCream = `${lightCream}`,
+      darkCream = `${darkCream}`,
+    },
+    primary: {
+      main: `${darkBlue}`
+    },
+    secondary: {
+      main: `${lightCream}`
+    }
+  }
+});
