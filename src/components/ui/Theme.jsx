1f89d3888d35449bed5d6ff8a46267073fc4d0c7
EsmaeilMIRZAEE <EsmaeilMIRZAEE@gmail.com>
2020-08-12 02:51:02 -0400

    Main colour--bug

--- src/components/ui/Theme.jsx
+++ src/components/ui/Theme.jsx
@@ -1,25 +1,25 @@
 import { createMuiTheme } from '@material-ui/core/styles';
 
-const darkBlue = "#9381FF"
-const lightBlue = "#B8B8FF"
-const silver = "#F8F7FF"
-const lightCream = "#FFEEDD"
-const darkCream = "#FFD8BE"
+const darkBlue = '#9381FF';
+const lightBlue = '#B8B8FF';
+const silver = '#F8F7FF';
+const lightCream = '#FFEEDD';
+const darkCream = '#FFD8BE';
 
 export default createMuiTheme({
   palette: {
     common: {
-      darkBlue = `${darkBlue}`,
-      lightBlue = `${lightBlue}`,
-      silver = `${silver}`,
-      lightCream = `${lightCream}`,
-      darkCream = `${darkCream}`,
+      darkBlue: `${darkBlue}`,
+      lightBlue: `${lightBlue}`,
+      silver: `${silver}`,
+      lightCream: `${lightCream}`,
+      darkCream: `${darkCream}`,
     },
     primary: {
-      main: `${darkBlue}`
+      main: `${darkBlue}`,
     },
     secondary: {
-      main: `${lightCream}`
-    }
-  }
+      main: `${lightCream}`,
+    },
+  },
 });
