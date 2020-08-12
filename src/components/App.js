d8758070bc939d8d1b4c97321fcdbb9416b7cefa
EsmaeilMIRZAEE <EsmaeilMIRZAEE@gmail.com>
2020-08-12 05:20:03 -0400

    init

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Header from './ui/Header';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <div className='App'>Hello</div>
+      {/* {[...new Array(1200)] */}
+      {/* .map( */}
+      {/* () => */}
+      {/* `Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsa enim quibusdam quaerat id cumque minima provident. Rerum fugiat aliquam suscipit dolore, quaerat nisi tempora sint ipsum natus saepe. Laborum, consequuntur.` */}
+      {/* ) */}
+      {/* .join('\n')} */}
+    </>
+  );
+}
+
+export default App;
