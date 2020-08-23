021453059934df651539d27acb3bfe28b760c379
EsmaeilMIRZAEE <EsmaeilMIRZAEE@gmail.com>
2020-08-23 13:42:59 -0400

    Add footer links--style

--- src/components/ui/Footer.jsx
+++ src/components/ui/Footer.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 import { Grid } from '@material-ui/core';
+import { Link } from 'react-router-dom';
 
 const useStyle = makeStyles((theme) => ({
   footer: {
