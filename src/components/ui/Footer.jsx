f6ba7a7a3c4dff454ca0bbec37eaae3679af61e5
EsmaeilMIRZAEE <EsmaeilMIRZAEE@gmail.com>
2020-08-23 03:54:37 -0400

    Add footer--bug

--- src/components/ui/Footer.jsx
+++ src/components/ui/Footer.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
 
 const useStyle = makeStyles((theme) => {
   footer: {
