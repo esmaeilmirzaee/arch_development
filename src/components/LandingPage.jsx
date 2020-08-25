605cbf3aaa8fc373ac061cc232f0f0193ea336f1
Esmaeil MIRZAEE <esmaeilmirzaee@gmail.com>
2020-08-25 16:06:52 -0400

    Technologies--bug

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,24 +1,24 @@
 import React from 'react';
 import { Grid, Typography } from '@material-ui/core/';
 import { FaHandHoldingHeart } from 'react-icons/fa';
-import bootstrap from '../assets/img/logos/bxl-bootstrap';
-import css from '../assets/img/logos/bxl-css3';
-import figma from '../assets/img/logos/bxl-figma';
-import git from '../assets/img/logos/bxl-git';
-import html from '../assets/img/logos/bxl-html5';
-import javascript from '../assets/img/logos/bxl-javascript';
-import react from '../assets/img/logos/bxl-react';
-import redux from '../assets/img/logos/bxl-redux';
-import sass from '../assets/img/logos/bxl-sass';
-import docker from '../assets/img/logos/docker';
-import rust from '../assets/img/logos/rust';
-import spring from '../assets/img/logos/springio';
-import elastic from '../assets/img/logos/elastic';
-import golang from '../assets/img/logos/golang';
-import java from '../assets/img/logos/java';
-import jetbrains from '../assets/img/logos/jetbrains';
-import mongodb from '../assets/img/logos/mongodb';
-import postgres from '../assets/img/logos/postgres';
+import bootstrap from '../assets/img/logos/bxl-bootstrap.svg';
+import css from '../assets/img/logos/bxl-css3.svg';
+import figma from '../assets/img/logos/bxl-figma.svg';
+import git from '../assets/img/logos/bxl-git.svg';
+import html from '../assets/img/logos/bxl-html5.svg';
+import javascript from '../assets/img/logos/bxl-javascript.svg';
+import react from '../assets/img/logos/bxl-react.svg';
+import redux from '../assets/img/logos/bxl-redux.svg';
+import sass from '../assets/img/logos/bxl-sass.svg';
+import docker from '../assets/img/logos/docker.svg';
+import rust from '../assets/img/logos/rust.svg';
+import spring from '../assets/img/logos/springio.svg';
+import elastic from '../assets/img/logos/elastic.svg';
+import golang from '../assets/img/logos/golang.svg';
+import java from '../assets/img/logos/java.svg';
+import jetbrains from '../assets/img/logos/jetbrains.svg';
+import mongodb from '../assets/img/logos/mongodb.svg';
+import postgres from '../assets/img/logos/postgres.svg';
 
 export default function LandingPage() {
   return (
